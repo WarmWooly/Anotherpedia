2ae1a2ad543e1d218b9411bebe69cbd4a38fb226
WarmWooly <68318069+WarmWooly@users.noreply.github.com>
2026-01-02 18:34:31 -0500

    Reverted page of the day

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,5 +1,5 @@
 // Warm_Wooly
-// 12/30/25 v1.250
+// 1/2/26 v1.251
 // Get constant variables from pages.js
 const PAGE = PAGESTORAGE
 const REDIRECT = REDIRECTSTORAGE
@@ -218,7 +218,7 @@ function seededRandom() {
   return x - Math.floor(x);
 }
 var randomIndex = Math.floor(seededRandom() * Object.keys(PAGE).length);
-var pageoftheday = "new year's day"; //Object.keys(PAGE)[randomIndex];
+var pageoftheday = Object.keys(PAGE)[randomIndex];
 
 // Adds page of the day text to the page of the day
 PAGE[pageoftheday].content = "{{b⭐ PAGE OF THE DAY! ⭐}}&sp" + PAGE[pageoftheday].content
